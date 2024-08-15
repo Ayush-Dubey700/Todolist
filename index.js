8a92122efdf8991e1887425f1e8b0f0169f4ec1f
Ayush Dubey <ayush8109140741@gmail.com>
2024-08-15 17:16:26 +0530

    Updated connection string for PostgreSQL and deployed to Render

--- index.js
+++ index.js
@@ -3,29 +3,24 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "permalist",
-  password: "Ayush@123",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
-
 app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
-    items = result.rows;
+    const items = result.rows;
 
     res.render("index.ejs", {
       listTitle: "Today",
@@ -38,7 +33,6 @@ app.get("/", async (req, res) => {
 
 app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  // items.push({title: item});
   try {
     await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
     res.redirect("/");
@@ -50,7 +44,6 @@ app.post("/add", async (req, res) => {
 app.post("/edit", async (req, res) => {
   const item = req.body.updatedItemTitle;
   const id = req.body.updatedItemId;
-
   try {
     await db.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
     res.redirect("/");
